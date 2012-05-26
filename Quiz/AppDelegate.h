1afab209c93a827b658bc693b79f2d273c5abcbc
Calvin Cheng <calvin@calvinx.com>
2012-05-26 14:47:34 +0800

    First commit

--- Quiz/AppDelegate.h
+++ Quiz/AppDelegate.h
@@ -9,7 +9,24 @@
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
+{
+    //  These instance variables are all pointers because of *
+    
+    int currentQuestionIndex;
+    
+    // The model objects
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    // The view objects
+    IBOutlet UILabel *questionField;
+    IBOutlet UILabel *answerField;
+    UIWindow *window;  // why dont need to declare IBOutlet here
+    
+}
+@property (retain, nonatomic) IBOutlet UIWindow *window;
 
-@property (strong, nonatomic) UIWindow *window;
+-(IBAction)showQuestion:(id)sender;
+-(IBAction)showAnswer:(id)sender;
 
 @end
