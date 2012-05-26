5e3e88b74da17dd5ec9d4df93134466042282d58
Calvin Cheng <calvin@calvinx.com>
2012-05-26 13:55:42 +0800

    Initial Commit

---
+++ QuizTests/QuizTests.h
@@ -0,0 +1,13 @@
+//
+//  QuizTests.h
+//  QuizTests
+//
+//  Created by Calvin Cheng on 26/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QuizTests : SenTestCase
+
+@end
---
+++ QuizTests/QuizTests.m
@@ -0,0 +1,32 @@
+//
+//  QuizTests.m
+//  QuizTests
+//
+//  Created by Calvin Cheng on 26/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "QuizTests.h"
+
+@implementation QuizTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in QuizTests");
+}
+
+@end
